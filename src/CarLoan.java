c8e56208854d73dcbdd8f4155ddad76cff1394cd
Javier Trujillo <rivera.dev@outlook.com>
2024-10-07 23:25:54 -0600

    Added - validation() method

--- src/CarLoan.java
+++ src/CarLoan.java
@@ -5,5 +5,14 @@ public class CarLoan {
     int interestRate = 5;
     int downPayment = 2000;
 
+    public void validation(){
+        if (loanLength <= 0 || interestRate <= 0){
+            System.out.println("Error! You must take out a valid car loan");
+        }
+    }
+    public static void main(String[] args){
+
+
+    }
 
 }
