db352da8fd93b2750b77a445738b93eca307a89a
Javier Trujillo <rivera.dev@outlook.com>
2024-10-07 23:37:51 -0600

    Added - Constructor method

--- src/CarLoan.java
+++ src/CarLoan.java
@@ -5,9 +5,24 @@ public class CarLoan {
     int interestRate = 5;
     int downPayment = 2000;
 
+    //constructor method.
+    public CarLoan(){
+
+    }
+
     public void validation(){
+
         if (loanLength <= 0 || interestRate <= 0){
             System.out.println("Error! You must take out a valid car loan");
+        } else if(carLoan < downPayment){
+            System.out.println("The car can be pais in full");
+        } else {
+            int remainingBalance = carLoan - downPayment;
+            int months = loanLength * 12;
+            int monthlyBalance = remainingBalance / months;
+            int interest = monthlyBalance * interestRate / 100;
+            int monthlyPayment = monthlyBalance + interest;
+            System.out.println(monthlyPayment);
         }
     }
     public static void main(String[] args){
